9a11bd9fad9ff38d8ac1d326b6487c8fe94e3c6a
WM <goldendeer6984@gmail.com>
2026-04-25 18:58:19 +1000

    gets global position; TODO: convert lat, long -> metres

--- run.bash
+++ run.bash
@@ -12,7 +12,7 @@ gnome-terminal --tab --title='Bridge' -- sh -c "ros2 run ros_gz_bridge parameter
 gnome-terminal --tab --title='Node' -- sh -c "ros2 run vision pycam"
 # ros2 run vision pycam
 
-gnome-terminal --tab --title='Node' -- sh -c "ros2 run offboard_control control"
+# gnome-terminal --tab --title='Node' -- sh -c "ros2 run offboard_control control"
 # ros2 run offboard_control control
 
 gnome-terminal --tab --title='GZ' -- sh -c "cd $HOME/PX4-Autopilot/; PX4_GZ_WORLD=aspa135_m3 PX4_GZ_MODEL_POSE="5.05,3.24,32.31,0,0,0" make px4_sitl gz_x500_segment_cam_down"
--- src/offboard_control/src/control.cpp
+++ src/offboard_control/src/control.cpp
@@ -1,46 +1,11 @@
-/****************************************************************************
- *
- * Copyright 2020 PX4 Development Team. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
 /**
- * @brief Offboard control example
- * @file offboard_control.cpp
- * @addtogroup examples
- * @author Mickey Cowden <info@cowden.tech>
- * @author Nuno Marques <nuno.marques@dronesolutions.io>
+ * @brief Offboard control adapted from PX4 Offboard control example (Mickey Cowden <info@cowden.tech>, Nuno Marques <nuno.marques@dronesolutions.io>)
  */
 
 #include <px4_msgs/msg/offboard_control_mode.hpp>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/vehicle_command.hpp>
+#include <px4_msgs/msg/vehicle_global_position.hpp>
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/msg/debug_vect.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -52,6 +17,7 @@
 using namespace std::chrono;
 using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
+using std::placeholders::_1;
 
 class OffboardControl : public rclcpp::Node
 {
@@ -62,6 +28,10 @@ public:
 		offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
 		trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
 		vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+    rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
+    auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
+		vehicle_position_subscriber_ = this->create_subscription<VehicleGlobalPosition>("/fmu/out/vehicle_global_position", qos,
+      std::bind(&OffboardControl::position_callback, this, _1));
 		
 		
 		offboard_setpoint_counter_ = 0;
@@ -70,28 +40,18 @@ public:
 			static float i = 0;
 
 			// Get target coord
-			// rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
-			// auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
 			
-			float x, y, z = 0.0;
-			float a = 5.0; float p = 20.0;
-			x = 4*a/p *  std::abs(std::fmod((i-p/4.0), p) -p/2.0);
-			y = i;
-			z = -5.0;
-			i += 0.3;
-			// subscription_ = this->create_subscription<px4_msgs::msg::DebugVect>("/fmu/in/debug_vect", qos,
-			// [this, x, y, z](const px4_msgs::msg::DebugVect::UniquePtr msg) mutable {
-			// 	x = 1.0;
-			// 	y = 2.0;
-			// 	z = 3.0;
-			// });
-			// Reading, but not propogating out to local variables outside lambda function:
-			// 
+			// float x, y, z = 0.0;
+			// float a = 5.0; float p = 20.0;
+			// x = 4*a/p *  std::abs(std::fmod((i-p/4.0), p) -p/2.0);
+			// y = i;
+			// z = -5.0;
+			// i += 0.3;
 
-			std::cout << "============================="   << std::endl;
-			std::cout << "x: " << x  << std::endl;
-			std::cout << "y: " << y  << std::endl;
-			std::cout << "z: " << z  << std::endl;
+			// std::cout << "============================="   << std::endl;
+			// std::cout << "x: " << x  << std::endl;
+			// std::cout << "y: " << y  << std::endl;
+			// std::cout << "z: " << z  << std::endl;
 
 			if (offboard_setpoint_counter_ == 10) {
 				// Change to Offboard mode after 10 setpoints
@@ -103,7 +63,7 @@ public:
 
 			// offboard_control_mode needs to be paired with trajectory_setpoint
 			publish_offboard_control_mode();
-			publish_trajectory_setpoint(x, y, -5.0);
+			// publish_trajectory_setpoint(x, y, -5.0);
 
 			// stop the counter after reaching 11
 			if (offboard_setpoint_counter_ < 11) {
@@ -123,7 +83,7 @@ private:
 	rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
 	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
-	rclcpp::Subscription<px4_msgs::msg::DebugVect>::SharedPtr subscription_;
+	rclcpp::Subscription<VehicleGlobalPosition>::SharedPtr vehicle_position_subscriber_;
 
 	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
 
@@ -132,6 +92,7 @@ private:
 	void publish_offboard_control_mode();
 	void publish_trajectory_setpoint(float x, float y, float z);
 	void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
+  void position_callback(VehicleGlobalPosition msg);
 };
 
 /**
@@ -204,6 +165,22 @@ void OffboardControl::publish_vehicle_command(uint16_t command, float param1, fl
 	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	vehicle_command_publisher_->publish(msg);
 }
+
+/**
+ * @brief Subscribe vehicle position
+ * @param 
+ */
+void OffboardControl::position_callback(const VehicleGlobalPosition msg)
+{
+  // [this, x, y, z](const px4_msgs::msg::DebugVect::UniquePtr msg)
+  std::string out_log = "Pos: " + std::to_string(msg.lat) + " " + 
+  std::to_string(msg.lon) + " " + 
+  std::to_string(msg.alt) + "\n";
+
+  RCLCPP_INFO(this->get_logger(), "Received position message");
+  RCLCPP_INFO(this->get_logger(), out_log.c_str());
+}
+
 
 int main(int argc, char *argv[])
 {
