9aa1e919b17a62b98b6eb6f27b552cfe9c967d6e
WM <goldendeer6984@gmail.com>
2026-05-17 21:37:57 +1000

    Attribution

--- src/vision/vision/unet/unet_parts.py
+++ src/vision/vision/unet/unet_parts.py
@@ -1,3 +1,25 @@
+# U-Net: Semantic segmentation with PyTorch
+# Copyright (C) milesial
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Original author: milesial
+# Original source: https://github.com/milesial/Pytorch-UNet
+
+# Modified: 17/05/2026
+# Last modified by: William Ma
+
 """ Parts of the U-Net model """
 
 import torch
