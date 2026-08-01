0ceda217a95afac9ca44cced990028113afe94ea
WM <goldendeer6984@gmail.com>
2026-08-01 22:15:34 +1000

    fix bash scripts

--- runControl.bash
+++ runControl.bash
@@ -7,7 +7,7 @@ colcon build
 source /opt/ros/humble/setup.bash
 source install/local_setup.bash
 
-# gnome-terminal --tab --title='Bridge' -- sh -c "ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=bridge_config/sensor_bridge.yaml"
+# gnome-terminal --tab --title='Bridge' -- bash -ic "ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=bridge_config/sensor_bridge.yaml"
 
 # ros2 run offboard_control control
 ros2 run offboard_control coord_advertiser
--- runVision.bash
+++ runVision.bash
@@ -7,5 +7,9 @@ colcon build
 source /opt/ros/humble/setup.bash
 source install/local_setup.bash
 
+gnome-terminal --tab --title='Bridge' -- bash -ic "ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=bridge_config/sensor_bridge.yaml"
+
+gnome-terminal --tab --title='GZ' -- bash -ic "cd $HOME/PX4-Autopilot/; HEADLESS=1 PX4_GZ_WORLD=aspa135_m3 PX4_GZ_MODEL_POSE="5.05,3.24,32.31,0,0,0" make px4_sitl gz_x500_segment_cam_down"
+
 ros2 run vision predictCam
 # ros2 run vision pycam
